4ceb85e29a328dde873947874adbfacfc2aa5027
Nishant Racherla <nishant.racherla@gmail.com>
2023-02-09 12:09:37 -0800

    added js fibonacci

---
+++ algos/fibonacci.js
@@ -0,0 +1,10 @@
+const fibonacci = (n, res = [0, 1]) => {
+  if (n === 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+  res.push(res.at(-1) + res.at(-2));
+  fibonacci(n - 1, res);
+  return res;
+}
+
+// console.log(fibonacci(5));
