39d1ae7a25c1f3ad476b5bb5d9e3c3858da647e5
Nishant Racherla <nishant.racherla@gmail.com>
2023-02-18 09:06:51 +0530

    added #1

---
+++ leetcode/1-two-sum.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/two-sum/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [i, map.get(target - nums[i])];
+    }
+    map.set(nums[i], i)
+  }
+};
