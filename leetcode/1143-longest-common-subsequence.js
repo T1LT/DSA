fc13bcbce26ea1951d41c1e476b5d3f65ed0a8fa
Nishant Racherla <nishant.racherla@gmail.com>
2023-03-02 12:08:58 +0530

    added 1143

---
+++ leetcode/1143-longest-common-subsequence.js
@@ -0,0 +1,28 @@
+// leetcode.com/problems/longest-common-subsequence/
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+https: var longestCommonSubsequence = function (
+  text1,
+  text2,
+  i = 0,
+  j = 0,
+  memo = {}
+) {
+  const key = i + "," + j;
+  if (key in memo) return memo[key];
+  if (i === text1.length || j === text2.length) return 0;
+  let res = 0;
+  if (text1[i] === text2[j]) {
+    res = longestCommonSubsequence(text1, text2, i + 1, j + 1, memo) + 1;
+  } else {
+    res = Math.max(
+      longestCommonSubsequence(text1, text2, i + 1, j, memo),
+      longestCommonSubsequence(text1, text2, i, j + 1, memo)
+    );
+  }
+  return (memo[key] = res);
+};
