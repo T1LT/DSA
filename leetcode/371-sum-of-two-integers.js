894a557a846dd42a91c3ee306c1675021d68d445
Nishant Racherla <nishant.racherla@gmail.com>
2023-03-31 10:27:05 -0700

    added 371

---
+++ leetcode/371-sum-of-two-integers.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/sum-of-two-integers/
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+  while (b != 0) {
+    let temp = (a & b) << 1;
+    a = a ^ b;
+    b = temp;
+  }
+  return a;
+};
