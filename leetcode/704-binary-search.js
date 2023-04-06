712d743d283dff18e73057f78fb53bba623d168d
Nishant Racherla <nishant.racherla@gmail.com>
2023-04-06 11:21:52 -0700

    added 704

---
+++ leetcode/704-binary-search.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/binary-search/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let left = 0, right = nums.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+  return -1;
+};
