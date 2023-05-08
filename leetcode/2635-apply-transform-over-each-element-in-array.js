092e355a0246aa93941ba62c67f29ffc59257f20
Nishant Racherla <nishant.racherla@gmail.com>
2023-05-07 18:42:39 -0700

    added 2635

---
+++ leetcode/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/apply-transform-over-each-element-in-array/description/
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+  const res = [];
+  for (let i = 0; i < arr.length; i++) {
+    res.push(fn(arr[i], i));
+  }
+  return res;
+};
