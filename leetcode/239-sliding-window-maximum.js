68f0fd2064be055556573305f8c11059ea41a48e
Nishant Racherla <nishant.racherla@gmail.com>
2023-08-14 15:43:46 -0700

    add 239

---
+++ leetcode/239-sliding-window-maximum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+  let l = 0,
+    r = 0;
+  const res = [];
+  const queue = [];
+  while (r < nums.length) {
+    while (queue.length && nums[queue.at(-1)] < nums[r]) queue.pop();
+    queue.push(r);
+    if (l > queue[0]) queue.shift();
+    if (r + 1 >= k) {
+      res.push(nums[queue[0]]);
+      l++;
+    }
+    r++;
+  }
+  return res;
+};
