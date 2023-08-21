ba3bd712010df871e99c30c462daefedb01c6fb2
Nishant Racherla <nishant.racherla@gmail.com>
2023-08-21 15:48:25 -0700

    Change 20

--- leetcode/20-valid-parantheses.js
+++ leetcode/20-valid-parantheses.js
@@ -1,18 +1,15 @@
-// leetcode.com/problems/valid-parentheses/
-
 /**
  * @param {string} s
  * @return {boolean}
  */
-const brackets = { ")": "(", "]": "[", "}": "{" };
-
-var isValid = function(str) {
-  const stack = [];
-  for (const char of str) {
+var isValid = function (s) {
+  const pairs = { ")": "(", "]": "[", "}": "{" };
+  let stack = [];
+  for (let char of s) {
     if (char === "(" || char === "[" || char === "{") stack.push(char);
     else {
-      if (brackets[char] !== stack.pop()) return false;
+      if (stack.pop() !== pairs[char]) return false;
     }
   }
-  return stack.length === 0;
+  return !stack.length;
 };
